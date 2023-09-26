2d0cd3724464c688427c201b8c772f30b3a70de6
Mariano D'Angelo <mariano.dangelo@claro.com.ar>
2023-09-26 20:27:38 -0300

    testing

--- SysAcadAppCons/Program.cs
+++ SysAcadAppCons/Program.cs
@@ -3,9 +3,9 @@
     internal class Program
     {
         //console project for testing purposes
-        static void Main(string[] args)
-        {
+        static void Main(string[] args){
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Testing Purposes");
         }
     }
 }
